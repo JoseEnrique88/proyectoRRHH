943f8a2c372e97f7ed91233873533d103eb1558d
José Enrique Briones <84800810+JoseEnrique88@users.noreply.github.com>
2024-04-29 15:49:18 -0500

    third commit

--- Modulos/Ficha/FichaTrabajador.Designer.vb
+++ Modulos/Ficha/FichaTrabajador.Designer.vb
@@ -595,7 +595,7 @@ Partial Class FichaTrabajador
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(576, 192)
+        Me.Button1.Location = New System.Drawing.Point(576, 255)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(68, 23)
         Me.Button1.TabIndex = 18
